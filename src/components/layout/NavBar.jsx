fd585836ea08e1ba73a3aba1bc9b6be17f929062
unknown <edgarrios412@gmail.com>
2024-12-14 22:54:30 -0500

    push

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -118,7 +118,7 @@ const NavBar = () => {
 
                         {/* Footer fijo con total y botón */}
                         <div className="sticky bottom-0 bg-white pt-4 pb-6 border-t border-gray-200">
-                                <h2 className="font-bold text-lg">Total: ${carrito.reduce((acc, curr) => acc + curr.precio * curr.cantidad, 0).toLocaleString()}</h2>
+                                <h2 className="font-bold text-lg">Total: ${carrito?.reduce((acc, curr) => acc + curr.precio * curr.cantidad, 0).toLocaleString()}</h2>
                                 <div className={`flex flex-row items-center gap-1 my-2`}>
                                         <img
                                             src={moneda}
@@ -127,10 +127,10 @@ const NavBar = () => {
                                         <span className={`text-sm text-blue-500 font-bold block`}>Por tu compra recibes {carrito?.reduce((acc, curr) => acc + curr.precio * curr.cantidad, 0)/1000} puntos</span>
                                     </div>
                                 {carrito?.length > 0 ? <a
-                                    href={`https://api.whatsapp.com/send/?phone=573022536253&text=Detalles del pedido%0A%0A${carrito.reduce(
+                                    href={`https://api.whatsapp.com/send/?phone=573022536253&text=Detalles del pedido%0A%0A${carrito?.reduce(
                                         (acc, curr) => acc + `${curr.nombreTienda} - ${curr.nombre} x${curr.cantidad}%0A`,
                                         ""
-                                    )}%0ATotal a pagar: $${carrito.reduce((acc, curr) => acc + curr.precio * curr.cantidad, 0).toLocaleString()}`}
+                                    )}%0ATotal a pagar: $${carrito?.reduce((acc, curr) => acc + curr.precio * curr.cantidad, 0).toLocaleString()}`}
                                     target="_blank"
                                 >
                                     <Button className="mt-2 w-full bg-blue-500 hover:bg-blue-600">Hacer pedido</Button>
